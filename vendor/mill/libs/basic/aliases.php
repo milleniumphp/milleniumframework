97da5b31091828d013871a9533698abd71ad612c
superhabber <superhabber@gmail.com>
2018-07-04 22:21:40 +0300

    vendor update

--- vendor/mill/libs/basic/aliases.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('WWW', dirname(__DIR__, 4).'/public');
-define('CORE', dirname(__DIR__, 4).'/vendor/mill/core');
-define('ROOT', dirname(__DIR__, 4));
-define('APP', dirname(__DIR__, 4). '/app');
-define('SECTIONS', dirname(__DIR__, 4). '/sections');
-define('LIBS', dirname(__DIR__, 4). '/vendor/mill/libs');
-define('CACHE', dirname(__DIR__, 4). '/tmp/cache');
-define('LAYOUT', 'default');
--- vendor/vlucas/valitron/tests/lang/en.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$original = include __DIR__ . '/../../lang/en.php';
-$original['required'] = 'A value is required for {field}';
-return $original;
