3ba6f357aafd526ba0f6b924dcba8cecb39e291b
superhabber <superhabber@gmail.com>
2018-07-09 16:50:06 +0300

    again

--- config/scripts.php
+++ config/scripts.php
@@ -4,7 +4,7 @@ return [
         '/assets/bootstrap/css/bootstrap.css',
     ],
     'js' => [
-        'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+        'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js',
         '/assets/bootstrap/js/bootstrap.min.js',
         '/assets/mill/system/js/main.js',
     ]
--- db/migrations/20180704190049_create_users.php
+++ db/migrations/20180704190049_create_users.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * default migration by Phinx Migration
+ * migration for user table for authentication system
+ * enter 'vendor/bin/phinx migrate -e development' into your console
+ */
 
 use Phinx\Migration\AbstractMigration;
 
@@ -8,10 +12,14 @@ class CreateUsers extends AbstractMigration
 
     public function change()
     {
-        $table = $this->table('users_test');
+        $table = $this->table('user');
         $table->addColumn('name', 'string', ['limit' => 255])  
+              ->addColumn('login', 'string', ['limit' => 255])
+              ->addColumn('password', 'string', ['limit' => 255])
+              ->addColumn('email', 'string', ['limit' => 255])
+              ->addColumn('role', 'enum', ['values' => ['user', 'admin']])
               ->addColumn('updated_at', 'datetime')
               ->addColumn('created_at', 'datetime',['null' => true])
-              ->create();
+        ->create();
     }
 }
