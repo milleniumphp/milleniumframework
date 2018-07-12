adc08f753deb98ba2c08855178508c9e00072cdc
superhabber <superhabber@gmail.com>
2018-07-12 13:22:07 +0300

    tests 25

--- tests/acceptance/HomeCest.php
+++ tests/acceptance/HomeCest.php
@@ -3,13 +3,6 @@ class HomeCest
 {
     public function ensureThatHomePageWorks(AcceptanceTester $I)
     {
-        $I->amOnPage('/pages/index');        
-        $I->see('Millenium Framework');
         
-        $I->seeLink('About us');
-        $I->click('About us');
-        $I->wait(2); // wait for page to be opened
-        
-        $I->see('Now you are in this file:');
     }
 }
